0280a7129318a37acb660cb7158bf3d0d5563cab
Moringa School <valentinecherutich7@email.com>
2022-10-03 16:36:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+let x;
+function superbowlWin(record){
+    x = record.find(({result}) => result ==='W' );
+if(x === undefined){
+    return;      
+}else if(x.year === '2015'){
+     return x.year;
+}
+}
